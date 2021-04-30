16a835f8a98c02efd14df4242bff7d631f1b0d2a
BlasToth <balazstothlaszlo+github@gmail.com>
2021-04-30 18:55:04 +0200

    build: random cards fill system

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const columnRight = document.querySelector('.column-right');
 
-const symbol = "🔮";
+const array = ["🐻", "🐼", "🐿️", "🐹", "🦝", "🐪", "🐨", "🐙", "🕷️"];
+const symbol = array[Math.floor(Math.random() * array.length)];
+const symbolIndex = array.indexOf(symbol);
+const arrayRest = array.splice(symbolIndex, 1);
 
-for (let i = 0; i < 100; i++) {
+
+for (let i = 99; i >= 0; i--) {
     let card = document.createElement("div");
     card.classList.add("cards")
-    card.innerText = `${i} ${symbol} `;
+    card.innerText = `${i}`;
+    if (i % 9 === 0 && i > 8 && i < 82) {
+        card.innerText = `${i} ${symbol} `;
+    } else {
+        card.innerText = `${i} ${array[Math.floor(Math.random() * array.length)]} `;
+
+    }
     columnRight.append(card);
 }
