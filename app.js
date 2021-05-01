c99a5572388c34f6081bd76fbceb57c508c7dc4d
BlasToth <balazstothlaszlo+github@gmail.com>
2021-05-01 15:18:14 +0200

    add transition to retry button and message

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function showSymbol() {
    const show = document.querySelector('.show-symbol');
    show.classList.add("show-symbol","faded-out");
    requestAnimationFrame(() => {
-    show.classList.remove("faded-out")
+    show.classList.remove("faded-out");
     setTimeout(function(){
         columnRight.innerHTML = `<p class="restart-message">Can't believe it? Click the button to try again.</p>`;
         const restartButton = document.createElement("button");
@@ -15,7 +15,15 @@ function showSymbol() {
         restartButton.classList.add("restart-button");
         columnRight.append(restartButton)
         restartButton.addEventListener("click", () => {window.location.reload()})
-    }, 2300);
+        const showRestartButton = document.querySelector(".restart-button");
+        showRestartButton.classList.add("restart-button", "faded-out");
+        const showRestartMessage = document.querySelector(".restart-message");
+        showRestartMessage.classList.add("restart-message", "faded-out");
+        requestAnimationFrame(() => {
+            showRestartButton.classList.remove("faded-out");
+            showRestartMessage.classList.remove("faded-out");
+        })
+    }, 1223);
     // TODO 
     // add shadow to the sphere
     
