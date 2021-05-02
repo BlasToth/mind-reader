feb199683cab1088c3c26278e26cb37021133af5
BlasToth <balazstothlaszlo+github@gmail.com>
2021-05-02 08:48:58 +0200

    refactor: change languages from CSS to JS + Add Hungarian

--- app.js
+++ app.js
@@ -7,41 +7,62 @@ const titleEnglish = document.querySelectorAll('.english');
 const titleSpanish = document.querySelectorAll('.spanish');
 const titleHungarian = document.querySelectorAll('.hungarian');
 const langs = document.querySelectorAll('div [lang]');
-console.log(langs)
+const title = document.querySelector('.title');
+const task = document.querySelectorAll('.title')[1];
+
+const titleStringSpanish = "Lector de pensamiento";
+const titleStringEnglish = "The NOT flash mind reader";
+const titleStringHungarian = "A nagy gondolatolvasó";
+const taskStringSpanish = `<p>Piensa un número cualquiera de dos cifras y suma ambos digitos.
+Después resta el resultado de la suma del número que pensaste.</p>
+
+<p class="stand-out-line">Por ejemplo: 42 (4 + 2 = 6) así 42 - 6 = 36</p>
+
+<p>Cuando tengas el número final búscalo en la tabla de números y
+fíjate en el animal que aperece a su lado. Concéntrate en el animal,
+y cuando lo veas con claridad en tu mente, haz click en la bola
+mágica.</p>`;
+const taskStringEnglish = `<p>Choose any two-digit number, add together both digits
+and then subtract the total from your original number.</p>
+
+<p class="stand-out-line">For example: 42 (4 + 2 = 6) so 42 - 6 = 36</p>
+
+<p>When you have the final number look it up on the chart
+and find the relevant animal. Concentrate on the animal
+and when you have it clearly in your mind click on the
+crystal ball and it will show you the animal you are
+thinking of.</p>`;
+const taskStringHungarian = `<p>Gondolj egy tetszőleges kétjegyű számra és add össze a számjegyeit!
+Ezt követően vond ki a gondolt számból a számjegyek összegét!</p>
+
+<p class="stand-out-line">Például: 42 (4 + 2 = 6) szóval 42 - 6 = 36</p>
+
+<p>Ha megvan az eredmény, keresd ki a táblázatból a számot és a mellette 
+található állatkát! Koncentrálj erősen a képre és ha megvan, kattints 
+a varázsgömbre!</p>`;
 
 
 
-function toggleLang(value) {
-    const currentLang = document.getElementsByClassName(value);
-    for (let i = 0; i < langs.length; i++) {
-        let splitArr = langs.split(".");
-        console.log(splitArr)
-        
-    }
-    
-    
+function toggleLangSp() {
+    title.innerText = titleStringSpanish;
+    task.innerHTML = taskStringSpanish;
+      
 }
-buttonSpanish.onclick = toggleLang("spanish");
-// buttonEnglish.onclick = toggleLang("english");
+function toggleLangEn() {
+    title.innerText = titleStringEnglish;
+    task.innerHTML = taskStringEnglish;
+      
+}
+function toggleLangHu() {
+    title.innerText = titleStringHungarian;
+    task.innerHTML = taskStringHungarian;
+      
+}
 
-function langHandlerSp() {
-    for (let i = 0; i < titleEnglish.length; i++) {
-        titleEnglish[i].classList.remove("visible");
-        titleEnglish[i].classList.add("not-visible");
-    }
-    for (let j = 0; j <titleSpanish.length; j++) {
-        titleSpanish[j].classList.add("visible");
-    }
-}
-function langHandlerEn() {
-    for (let i = 0; i < titleSpanish.length; i++) {
-        titleSpanish[i].classList.remove("visible");
-        titleSpanish[i].classList.add("not-visible");
-    }
-    for (let j = 0; j <titleEnglish.length; j++) {
-        titleEnglish[j].classList.add("visible");
-    }
-}
+buttonSpanish.onclick = toggleLangSp;
+buttonEnglish.onclick = toggleLangEn;
+buttonHungarian.onclick = toggleLangHu;
+
 
 
 function showSymbol() {
